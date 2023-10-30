bec5e4c115f2a81344e6a7431c916b347d825096
Nahid Hasan <117300018+Nahid396@users.noreply.github.com>
2023-10-31 00:35:31 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,71 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="box">
+         <img src="images/far3.jpg" alt="">
+         <h3>why choose us?</h3>
+         <p> Our Food items comes from Rural Area we make sure that no harmful chemical are used for the item also we care about the farmer & respect their hardship.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+      <div class="box">
+         <img src="images/Farmer1.jpg" alt="">
+         <h3>what we provide?</h3>
+         <p>We provide the Fresh food & without preservative also at low cost.</p>
+         <a href="shop.php" class="btn">our shop</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+
+
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>Nahid Hasan Sarker</span> | all rights reserved! </p>
+   <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a></br>
+      
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+
+</footer>
